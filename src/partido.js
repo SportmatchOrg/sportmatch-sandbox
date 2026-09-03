b55674aac84660f0087acad473c360e6d2d02d91
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2026-09-03 16:27:19 -0300

    chore: tercer push, valida edicion in-place

--- src/partido.js
+++ src/partido.js
@@ -5,3 +5,4 @@ function crearPartido(deporte, fecha, cupo) {
 module.exports = { crearPartido };
 // trigger de validación de reusables (2026-09-03T19:03:43Z)
 // segundo push, valida el fix del marcador (2026-09-03T19:26:13Z)
+// tercer push, valida edicion in-place (2026-09-03T19:27:19Z)

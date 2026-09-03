d3ae4b12582409329fd1b58bdcf12d255c2c2cfb
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2026-09-03 16:26:13 -0300

    chore: segundo push para validar el fix de comentarios

--- src/partido.js
+++ src/partido.js
@@ -4,3 +4,4 @@ function crearPartido(deporte, fecha, cupo) {
 }
 module.exports = { crearPartido };
 // trigger de validación de reusables (2026-09-03T19:03:43Z)
+// segundo push, valida el fix del marcador (2026-09-03T19:26:13Z)

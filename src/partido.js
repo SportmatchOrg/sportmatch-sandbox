25ba8a3817884c6e702be21d3ac5c06e07987326
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2026-09-03 16:32:40 -0300

    chore: quinto push

--- src/partido.js
+++ src/partido.js
@@ -7,3 +7,4 @@ module.exports = { crearPartido };
 // segundo push, valida el fix del marcador (2026-09-03T19:26:13Z)
 // tercer push, valida edicion in-place (2026-09-03T19:27:19Z)
 // cuarto push, valida -F body=@archivo (2026-09-03T19:29:11Z)
+// quinto push, confirma edicion en lugar de creacion

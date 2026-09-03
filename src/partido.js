61bdcac04174b87d35fc12fda8bf4bc978df671a
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2026-09-03 16:29:11 -0300

    chore: cuarto push, valida -F

--- src/partido.js
+++ src/partido.js
@@ -6,3 +6,4 @@ module.exports = { crearPartido };
 // trigger de validación de reusables (2026-09-03T19:03:43Z)
 // segundo push, valida el fix del marcador (2026-09-03T19:26:13Z)
 // tercer push, valida edicion in-place (2026-09-03T19:27:19Z)
+// cuarto push, valida -F body=@archivo (2026-09-03T19:29:11Z)

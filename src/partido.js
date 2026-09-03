8baf8cd06370ea4ba6315fc622633b1c65001ffb
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2026-09-03 15:24:16 -0300

    chore(qa-review): pasar secrets explícitos en vez de inherit

--- src/partido.js
+++ src/partido.js
@@ -3,3 +3,4 @@ function crearPartido(deporte, fecha, cupo) {
   return { deporte, fecha, cupo };
 }
 module.exports = { crearPartido };
+// trigger QA Review: validar secrets explicitos en el wrapper (2026-09-03T18:24:16Z)
